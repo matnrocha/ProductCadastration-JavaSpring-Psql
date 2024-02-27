a7286ece77ac97995d8e04857353985fdb38a642
matro0 <mateusanroc@gmail.com>
2024-02-27 02:24:06 -0300

    implements deleteProduct

--- demo/src/main/java/com/example/demo/controllers/ProductController.java
+++ demo/src/main/java/com/example/demo/controllers/ProductController.java
@@ -5,6 +5,7 @@ import com.example.demo.domain.product.Product;
 import com.example.demo.domain.product.ProductRepository;
 import com.example.demo.domain.product.RequestProductDTO;
 import com.example.demo.domain.product.RequestProductPutDTO;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -35,10 +36,24 @@ public class ProductController {
     }
 
     @PutMapping    //Not working yet
+    @Transactional      // More than 1 command SQL (all of nothing)
     public ResponseEntity updateProduct(@RequestBody @Validated RequestProductPutDTO data){
-        Optional<Product> product = repository.findById(data.id());
+        Optional<Product> optionalProduct = repository.findById(data.id());
+        if(optionalProduct.isPresent()){
+            Product product = optionalProduct.get();
+            product.setName(data.name());
+            product.setPrice_in_cents(data.price_in_cents());
+            return ResponseEntity.ok(product);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
-        return ResponseEntity.ok(product);
+    //geralmente não se deleta o dado permanentemente, mas se guarda para analytics ou restaurar versões
+    @DeleteMapping("/{id}")     //The id is in the route
+    public ResponseEntity deleteProduct(@PathVariable String id){
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
